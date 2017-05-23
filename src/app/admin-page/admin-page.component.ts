c765efefa573e5980d22d3273520bea5e2e9432b
Trần Trung Hiếu <hieutt.bka59@gmail.com>
2017-05-24 00:22:33 +0700

    test

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -10,4 +10,45 @@ export class AdminPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() { }
+
+  // Ok. Ví dụ nhé chúng ta sẽ có danh sách member ở đây:
+
+  members = [{
+    id: 1234,
+    name: "lsdfkjalsdkjfalsdkj"
+  }, {
+    id: 1213,
+    name: "lsdfkjalsdkjfalsdkj"
+  }, {
+    id: 432,
+    name: "lsdfkasdfdjfalsdkj"
+  }, {
+    id: 423432,
+    name: "lsdfkjalsdkjfalsdkj"
+  }]
+
+  // Và chúng ta có danh sách id các editor ở dưới này
+
+  editors = [1234, 432]
+
+  addEditor(member) {
+    // Nếu không tìm thấy id của member ở trong danh sách các editors
+    if (this.editors.findIndex(_id => _id === member.id) === -1) {
+      // Thì chúng ta sẽ thêm id của member đó vào danh sách các editor
+
+      this.editors.push(member.id);
+    }
+
+    console.log(this.editors);
+  }
+
+  removeEditor(editor) {
+    let editorId = this.editors.findIndex(_editor => _editor === editor);
+
+    if (editorId !== -1) {
+      this.editors.splice(editorId, 1);
+    }
+
+    console.log(this.editors)
+  }
 }
